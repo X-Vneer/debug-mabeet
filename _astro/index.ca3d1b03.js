4add7a76c58d67659c30c63e69797342c55eb2d9
X_V.neer <xv.neer@gmail.com>
2023-04-17 17:07:42 +0300

    updated files

---
+++ _astro/index.ca3d1b03.js
@@ -0,0 +1,4 @@
+import"./jsx-runtime.1aeea3a4.js";const t="/_astro/A1.0561a471.jpg",e="/_astro/A2.e6f2e6e9.jpg",i="/_astro/A3.b63a9075.jpg",l=[{title:"استراحات خاصة",link:"#"},{title:"شاليهات شاطئية",link:"#",preload:!0},{title:"شقق خاصة",link:"#"},{title:"شقق مطله",link:"#"},{title:" فلل خاصة",link:"#"},{title:"مخيمات",link:"#"},{title:"مزارع",link:"#"}],s=[...l,{title:"الشروط والأحكام",link:"#"},{title:"سياسة الخصوصية",link:"#"}],o=[{image:t,content:[{size:"lg",title:`اكتشف احدث
+عروض مبيت`}]},{image:e,content:[{size:"sm",title:"كاشت؟"},{size:"lg",title:`أبحث عن افضل
+المخيمات مع مبيت`}]},{image:i,content:[{size:"lg",title:`عروض خاصه
+لفتره اقامه اطول`},{size:"sm",title:"استمتع بعطلة طويلة!"}]}];export{l as h,s as m,o as s};
