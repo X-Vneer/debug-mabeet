8308b7d08bd5e2dfac61196c16998c68489a8b66
X_V.neer <xv.neer@gmail.com>
2023-04-18 04:16:35 +0300

    updated

---
+++ _astro/index.aaa923ba.js
@@ -0,0 +1,6 @@
+import"./jsx-runtime.1aeea3a4.js";const t="/_astro/A1.0561a471.jpg",e="/_astro/A2.e6f2e6e9.jpg",i="/_astro/A3.b63a9075.jpg",l="/_astro/A4.f91bee16.jpg",s="/_astro/A6.9768e930.jpg",n=[{title:"استراحات خاصة",link:"#"},{title:"شاليهات شاطئية",link:"#",preload:!0},{title:"شقق خاصة",link:"#"},{title:"شقق مطله",link:"#"},{title:" فلل خاصة",link:"#"},{title:"مخيمات",link:"#"},{title:"مزارع",link:"#"}],g=[...n,{title:"الشروط والأحكام",link:"#"},{title:"سياسة الخصوصية",link:"#"}],a=[{image:t,content:[{size:"sm",title:"اكتشف العلا!"},{size:"lg",title:`اكتشف العلا وروح السعودية
+ارض الحضارات البشريه`}]},{image:i,content:[{size:"sm",title:"اكتشف  الرياض"},{size:"lg",title:`استمتع بتجربة السكن التشاركي في الرياض
+وعيش اجواء الترفيه الموسميه .`}]},{image:e,content:[{size:"lg",title:`احجز واستمتع على شواطئ البحر الاحمر
+في جدة وعيش تجربة السكن`},{size:"sm",title:"عيش التجربة في جدة "}]},{image:l,content:[{size:"lg",title:` رتب حجزك لأطهر بقاع
+الارض بالسكن التشاركي في مكة .`},{size:"sm",title:"رتّبها لمكة "}]},{image:s,content:[{size:"sm",title:"احجز للشرقية!"},{size:"lg",title:`أينما تذهب في الشرقية
+تجد السحر والجمال والهدوء .`}]}];export{n as h,g as m,a as s};
